2199c5b32b952a7369df9e15fedddb71017bab1f
Ujjawal Kinker <ujjawal.kinker@ad.infosys.com>
2023-11-22 10:53:05 +0530

    jwt added

--- src/main/java/com/uj/config/SecurityConfig.java
+++ src/main/java/com/uj/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package com.uj.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.uj.filter.JWTFilter;
 import com.uj.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private JWTFilter filter;
 
 	@Bean
 	public UserDetailsService userDetailsService() {
@@ -33,12 +42,17 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.csrf().disable()
 		        .authorizeHttpRequests()
-		        .requestMatchers("/product/welcome","/product/save").permitAll()
+		        .requestMatchers("/product/welcome","/product/save","/product/generate").permitAll()
 		        .and()
 		        .authorizeHttpRequests()
 		        .requestMatchers("/product/**").authenticated()
-		        .and().formLogin()
-		        .and().build();
+		        .and()
+		        .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 	}
 	
 	@Bean
@@ -48,6 +62,11 @@ public class SecurityConfig {
 		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 		
 		return daoAuthenticationProvider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 	
 }
--- src/main/java/com/uj/service/UserService.java
+++ src/main/java/com/uj/service/UserService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.uj.entity.Users;
 import com.uj.repo.UsersRepo;
 
+@Component
 public class UserService implements UserDetailsService {
 	
 	@Autowired
